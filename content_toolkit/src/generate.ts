3374b49eb692ae6b75b0da885d10ab1832ef852f
Yusei Ito <me@yuseiito.com>
2022-03-27 00:35:58 +0900

    Remove: courcedeck

--- content_toolkit/src/generate.ts
+++ content_toolkit/src/generate.ts
@@ -1,8 +1,6 @@
 import { XMLParser } from 'fast-xml-parser';
 import fs from 'fs';
 import path from 'path';
-import { Cource } from './types/XMLTypes';
-import srcResolve from './srcResolve';
 
 if (process.argv.length < 4) {
   console.log('Usage: node generate.js [dest] [files...]');
@@ -13,13 +11,6 @@ import simplifyOputput from './XMLTypeConversion';
 
 const dest = process.argv[2];
 const files = process.argv.slice(3);
-
-type CourceDeck = {
-  cource: Cource;
-  file: string;
-};
-
-const cources: CourceDeck[] = [];
 
 for (const file of files) {
   const XMLStr = fs.readFileSync(file, 'utf8');
@@ -34,17 +25,10 @@ for (const file of files) {
 
   const parser = new XMLParser(options);
   const jObj = parser.parse(XMLStr);
-  const result = await srcResolve(path.dirname(file), simplifyOputput(jObj));
+  const result = simplifyOputput(jObj);
 
   fs.writeFileSync(
     path.resolve(process.cwd(), dest, `${path.basename(file, '.xml')}.json`),
     JSON.stringify(result, null, 2)
   );
-
-  cources.push({ file, cource: result[0] });
 }
-
-fs.writeFileSync(
-  path.resolve(process.cwd(), dest, `${path.basename('courcedeck')}.json`),
-  JSON.stringify({ cources }, null, 2)
-);
