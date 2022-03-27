c16ac247effc4e8bb637d74a70f2a95934c2d8b1
Yusei Ito <me@yuseiito.com>
2022-03-27 14:24:47 +0900

    Modify: Fetch cource content at runtime

--- preview/src/Cource.svelte
+++ preview/src/Cource.svelte
@@ -3,11 +3,11 @@
  import Lesson from './Lesson.svelte';
 import MultilingualText from './MultilingualText.svelte';
  export let courceWithMetadata;
- let {cource,file}=courceWithMetadata;
+ let {cource,meta:{source}}=courceWithMetadata;
 </script>
 <section class="column content mb-6">
  <h3><MultilingualText text={cource.title}/></h3>
- <h6>Source: <code>{file}</code></h6>
+ <h6>Source: <code>{source}</code></h6>
  <div class="row">
   {#each cource.lessons as lesson}
    <Lesson lesson={lesson}/>
--- preview/src/main.ts
+++ preview/src/main.ts
@@ -1,14 +1,7 @@
 import App from './App.svelte';
 
-import samplecourcedeck  from '../../generated/sample/courcedeck.json'
-import courcedeck  from '../../generated/articles/courcedeck.json'
-
 const app = new App({
 	target: document.body,
-	props: {
-		cources: courcedeck.cources,
-		sampleCources:samplecourcedeck.cources
-	}
 });
 
 export default app;
